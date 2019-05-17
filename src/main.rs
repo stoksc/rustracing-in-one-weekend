775dcb5aa91c8323ed2879e904bebd370ae5bb73
stoksc <bradley.laney@gmail.com>
2019-05-16 21:55:19 -0400

    A circle.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use rustracing::ray::Ray;
-use rustracing::vec3::Vec3;
+use rustracing::vec3::{self, Vec3};
+
+fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> bool {
+    let oc = ray.origin() - center;
+    let a = vec3::dot(ray.direction(), ray.direction());
+    let b = 2.0 * vec3::dot(&oc, ray.direction());
+    let c = vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
 
 fn color(ray: &Ray) -> Vec3 {
+    if hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, ray) {
+        return Vec3(1.0, 0.0, 0.0);
+    }
     let unit_direction = ray.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * &Vec3(1.0, 1.0, 1.0) + t * &Vec3(0.5, 0.7, 1.0);
+    (1.0 - t) * &Vec3(1.0, 1.0, 1.0) + t * &Vec3(0.5, 0.7, 1.0)
 }
 
 fn main() {
@@ -24,7 +36,7 @@ fn main() {
 
             let pixel = color(&Ray {
                 a: &origin,
-                b: &(u * horizontal + v * vertical + lower_left_corner),
+                b: &(&(u * horizontal + v * vertical) + lower_left_corner),
             });
 
             let ir = (pixel.r() * 255.99) as isize;
--- src/vec3.rs
+++ src/vec3.rs
@@ -3,19 +3,19 @@ use std::ops;
 #[derive(Clone)]
 pub struct Vec3(pub f32, pub f32, pub f32);
 
+pub fn dot(left: &Vec3, other: &Vec3) -> f32 {
+    left[0] * other[0] + left[1] * other[1] + left[2] * other[2]
+}
+
+pub fn cross(left: &Vec3, other: &Vec3) -> Vec3 {
+    Vec3(
+        left[1] * other[2] - left[2] * other[1],
+        left[2] * other[0] - left[0] * other[2],
+        left[0] * other[1] - left[1] * other[0],
+    )
+}
+
 impl Vec3 {
-    pub fn dot(&self, other: &Vec3) -> Vec3 {
-        Vec3(self[0] * other[0], self[1] * other[1], self[2] * self[2])
-    }
-
-    pub fn cross(&self, other: &Vec3) -> Vec3 {
-        Vec3(
-            self[1] * other[2] - self[2] * other[1],
-            self[2] * other[0] - self[0] * other[2],
-            self[0] * other[1] - self[1] * other[0],
-        )
-    }
-
     pub fn length(&self) -> f32 {
         (self[0] * self[0] + self[1] * self[1] + self[2] * self[2]).sqrt()
     }
@@ -61,14 +61,6 @@ impl ops::Add for Vec3 {
     }
 }
 
-impl ops::Add<&Vec3> for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, rhs: &Vec3) -> Vec3 {
-        Vec3(self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2])
-    }
-}
-
 impl ops::Add for &Vec3 {
     type Output = Vec3;
 
@@ -89,6 +81,14 @@ impl ops::Sub for Vec3 {
     type Output = Vec3;
 
     fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3(self[0] - rhs[0], self[1] - rhs[1], self[2] - rhs[2])
+    }
+}
+
+impl ops::Sub for &Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: &Vec3) -> Vec3 {
         Vec3(self[0] - rhs[0], self[1] - rhs[1], self[2] - rhs[2])
     }
 }
